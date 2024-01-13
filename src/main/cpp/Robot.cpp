d028fc9a2bab0c831af3c1cb952be554ab4fc8ce
Scott <114890628+skoot1331@users.noreply.github.com>
2024-01-13 13:50:59 -0800

    Revert "jagwires initial commit"
    This reverts commit a5f9edb0cfa230cc0c8c102245c4361e40152a7b.

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -4,33 +4,19 @@
 
 #include "Robot.h"
 
-#include <frc/DataLogManager.h>
-#include <frc/DriverStation.h>
-#include <frc/livewindow/LiveWindow.h>
 #include <frc2/command/CommandScheduler.h>
 
-void Robot::RobotInit() noexcept
-{
-  frc::LiveWindow::SetEnabled(false);
-  frc::LiveWindow::DisableAllTelemetry();
-
-  frc::DataLogManager::Start();
-  frc::DriverStation::StartDataLog(frc::DataLogManager::GetLog());
-}
+void Robot::RobotInit() {}
 
 /**
- * This function is called every robot packet, no matter the mode. Use
+ * This function is called every 20 ms, no matter the mode. Use
  * this for items like diagnostics that you want to run during disabled,
  * autonomous, teleoperated and test.
  *
  * <p> This runs after the mode specific periodic functions, but before
  * LiveWindow and SmartDashboard integrated updating.
  */
-// This performs the sense portion of sense/think/act, including sending test
-// mode telemetry.  It also handles think and act, except in test mode.  In the
-// latter case, TestPeriodic() handles manually driven act.
-void Robot::RobotPeriodic() noexcept
-{
+void Robot::RobotPeriodic() {
   frc2::CommandScheduler::GetInstance().Run();
 }
 
@@ -39,89 +25,56 @@ void Robot::RobotPeriodic() noexcept
  * can use it to reset any subsystem information you want to clear when the
  * robot is disabled.
  */
-void Robot::DisabledInit() noexcept
-{
-  m_container.TestExit();
+void Robot::DisabledInit() {}
 
-  m_container.DisabledInit();
-}
-
-void Robot::DisabledPeriodic() noexcept {}
-
-void Robot::DisabledExit() noexcept
-{
-  m_container.DisabledExit();
-}
+void Robot::DisabledPeriodic() {}
 
 /**
  * This autonomous runs the autonomous command selected by your {@link
  * RobotContainer} class.
  */
-void Robot::AutonomousInit() noexcept
-{
-  m_container.TestExit();
-
+void Robot::AutonomousInit() {
   m_autonomousCommand = m_container.GetAutonomousCommand();
 
-  if (m_autonomousCommand)
-  {
+  if (m_autonomousCommand) {
     m_autonomousCommand->Schedule();
   }
-
-  m_container.AutonomousInit();
 }
 
-void Robot::AutonomousPeriodic() noexcept {}
+void Robot::AutonomousPeriodic() {}
 
-void Robot::AutonomousExit() noexcept {}
-
-void Robot::TeleopInit() noexcept
-{
-  m_container.TestExit();
-
+void Robot::TeleopInit() {
   // This makes sure that the autonomous stops running when
   // teleop starts running. If you want the autonomous to
   // continue until interrupted by another command, remove
   // this line or comment it out.
-  if (m_autonomousCommand)
-  {
+  if (m_autonomousCommand) {
     m_autonomousCommand->Cancel();
-    m_autonomousCommand.reset();
   }
-
-  m_container.TeleopInit();
 }
 
 /**
  * This function is called periodically during operator control.
  */
-void Robot::TeleopPeriodic() noexcept {}
-
-void Robot::TeleopExit() noexcept {}
-
-void Robot::TestInit() noexcept
-{
-  m_container.TestInit();
-}
+void Robot::TeleopPeriodic() {}
 
 /**
  * This function is called periodically during test mode.
- * Note that test mode does not follow the command-based paradigm; it follows
- * Init/Periodic.
  */
-void Robot::TestPeriodic() noexcept
-{
-  m_container.TestPeriodic();
-}
+void Robot::TestPeriodic() {}
 
-void Robot::TestExit() noexcept
-{
-  m_container.TestExit();
-}
+/**
+ * This function is called once when the robot is first started up.
+ */
+void Robot::SimulationInit() {}
+
+/**
+ * This function is called periodically whilst in simulation.
+ */
+void Robot::SimulationPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
-int main()
-{
+int main() {
   return frc::StartRobot<Robot>();
 }
 #endif
--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -4,67 +4,32 @@
 
 #pragma once
 
-#include <frc2/command/button/CommandGenericHID.h>
-#include <frc2/command/button/CommandXboxController.h>
 #include <frc2/command/CommandPtr.h>
+#include <frc2/command/button/CommandXboxController.h>
 
-#include "commands/AutonomousCommands.h"
-#include "commands/TestModeCommands.h"
-#include "subsystems/DriveSubsystem.h"
-#include "subsystems/FeederSubsystem.h"
-#include "subsystems/Infrastructure.h"
-#include "subsystems/ShooterSubsystem.h"
-
-#include <memory>
-#include <tuple>
+#include "Constants.h"
+#include "subsystems/ExampleSubsystem.h"
 
 /**
  * This class is where the bulk of the robot should be declared.  Since
  * Command-based is a "declarative" paradigm, very little robot logic should
  * actually be handled in the {@link Robot} periodic methods (other than the
  * scheduler calls).  Instead, the structure of the robot (including subsystems,
- * commands, and button mappings) should be declared here.
+ * commands, and trigger mappings) should be declared here.
  */
-class RobotContainer
-{
-public:
-  RobotContainer() noexcept;
+class RobotContainer {
+ public:
+  RobotContainer();
 
-  RobotContainer(const RobotContainer &) = delete;
-  RobotContainer &operator=(const RobotContainer &) = delete;
+  frc2::CommandPtr GetAutonomousCommand();
 
-  std::optional<frc2::CommandPtr> GetAutonomousCommand() noexcept;
+ private:
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  frc2::CommandXboxController m_driverController{
+      OperatorConstants::kDriverControllerPort};
 
-  void TestInit() noexcept;
-  void TestExit() noexcept;
-  void TestPeriodic() noexcept;
+  // The robot's subsystems are defined here...
+  ExampleSubsystem m_subsystem;
 
-  void DisabledInit() noexcept;
-  void DisabledExit() noexcept;
-  void AutonomousInit() noexcept;
-  void TeleopInit() noexcept;
-
-private:
-  static frc2::CommandPtr DriveCommandFactory(RobotContainer *container) noexcept;
-  static frc2::CommandPtr PointCommandFactory(RobotContainer *container) noexcept;
-
-  std::tuple<double, double, double, bool> GetDriveTeleopControls() noexcept;
-
-  void ConfigureBindings() noexcept;
-
-  bool m_fieldOriented{false};
-  bool m_lock{false};
-  bool m_slow{false};
-  double m_shooterVelocity{0.0};
-  uint m_LEDPattern{29};
-  uint m_LEDPatternCount{0};
-
-  // The robot's subsystems and commands are defined here...
-  DriveSubsystem m_driveSubsystem;
-  FeederSubsystem m_feederSubsystem;
-  InfrastructureSubsystem m_infrastructureSubsystem;
-  ShooterSubsystem m_shooterSubsystem;
-
-  frc2::CommandXboxController m_xbox{0};
-  frc2::CommandGenericHID m_buttonBoard{1};
+  void ConfigureBindings();
 };
